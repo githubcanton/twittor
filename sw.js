4db5697e568668e58a3148339b8a6532ac7944cc
githubcanton <github.canton@gmail.com>
2018-11-22 00:28:12 +0100

    Cambios activate para limpiar dynamic cache

--- sw.js
+++ sw.js
@@ -51,6 +51,10 @@ self.addEventListener('activate',  e => {
                 return caches.delete(key);
             }
 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
         });
 
     });
